d1fdabcf3ab54c83f7e826967da5e2579e21f18c
Achmad Fauzi <achmadfauzi@live.com>
2022-03-19 14:27:52 +0700

    add foreign keys in table detail users

--- database/migrations/2022_03_19_061452_create_detail_user_table.php
+++ database/migrations/2022_03_19_061452_create_detail_user_table.php
@@ -15,8 +15,8 @@ class CreateDetailUserTable extends Migration
     {
         Schema::create('detail_user', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
-            $table->integer('type_user_id');
+            $table->foreignId('user_id')->nullable()->index('fk_detail_user_to_users');
+            $table->foreignId('type_user_id')->nullable()->index('fk_detail_user_to_type_user');
             $table->string('contact')->nullable();
             $table->longText('address')->nullable();
             $table->longText('photo')->nullable();
---
+++ database/migrations/2022_03_19_071136_add_foreign_keys_to_detail_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeysToDetailUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('detail_user', function (Blueprint $table) {
+            $table->foreign('user_id', 'fk_detail_user_to_users')->reference('id')->on('users')->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->foreign('type_user_id', 'fk_detail_user_to_type_user')->reference('id')->on('type_user')->onDelete('CASCADE')->onUpdate('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('detail_user', function (Blueprint $table) {
+            $table->dropForeign('fk_detail_user_to_users');
+            $table->dropForeign('fk_detail_user_to_type_user');
+        });
+    }
+}
