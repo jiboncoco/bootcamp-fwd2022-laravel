bcee8fff7bb6f953d2b12169e1dcd6e16d13e5ff
Achmad Fauzi <achmadfauzi@live.com>
2022-04-02 15:13:40 +0700

    add includes & add asset for fronsite

---
+++ resources/views/includes/frontsite/script.blade.php
@@ -0,0 +1,1 @@
+<script defer src="{{ url('https://unpkg.com/alpinejs@3.8.0/dist/cdn.min.js') }}"></script>
---
+++ resources/views/includes/frontsite/style.blade.php
@@ -0,0 +1,5 @@
+<link rel="preconnect" href="{{ url('https://fonts.googleapis.com') }}" />
+<link rel="preconnect" href="{{ url('https://fonts.gstatic.com') }}" crossorigin />
+<link href="{{ url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet') }}"/>
+
+<link rel="stylesheet" href="{{ asset('/assets/frontsite/style.css') }}" />
