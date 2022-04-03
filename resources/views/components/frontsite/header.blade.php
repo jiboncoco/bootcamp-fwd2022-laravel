cab488d262559b53262bb9ea53feb125fc79467b
Achmad Fauzi <achmadfauzi@live.com>
2022-04-03 13:40:51 +0700

    slicing section hero

--- resources/views/components/frontsite/header.blade.php
+++ resources/views/components/frontsite/header.blade.php
@@ -26,7 +26,7 @@
                     items-center px-1 pt-1 text-lg font-medium"
                 -->
                 <a
-                    href="#"
+                    href="{{ route('index') }}"
                     class="text-[#1E2B4F] relative after:absolute after:content-[''] after:border-b-2 after:border-[#0D63F5] after:w-8/12 after:-translate-x-1/2 after:bottom-3 after:left-1/2 font-semibold inline-flex items-center px-1 text-lg"
                 >
                     Home
@@ -56,7 +56,7 @@
             <!-- Button (no authenticated) -->
             <div class="hidden lg:ml-10 lg:flex lg:items-center">
                 <a
-                href="src/pages/sign-in.html"
+                href="{{ route('login') }}"
                 class="inline-flex items-center rounded-full text-[#1E2B4F] text-lg font-medium bg-[#F2F6FE] px-10 py-3"
                 >
                 Sign In
@@ -157,7 +157,7 @@
             class="py-3 border-gray-200"
         >
             <a
-                href="src/pages/sign-in.html"
+                href="{{ route('login') }}"
                 class="flex items-center justify-center text-center mx-4 rounded-full text-[#1E2B4F] text-lg font-medium bg-[#F2F6FE] px-10 py-3"
             >
             Sign In
--- resources/views/pages/frontsite/landing-page/index.blade.php
+++ resources/views/pages/frontsite/landing-page/index.blade.php
@@ -3,5 +3,78 @@
 @section('title', 'Home')
 
 @section('content')
-    Home
+
+    <!-- Content -->
+    <main class="min-h-screen">
+
+        <!-- Hero -->
+        <section class="relative mt-12">
+            <!-- Hero Image -->
+            <div class="hidden lg:block lg:absolute lg:z-10 lg:top-0 lg:right-0"
+            aria-hidden="true">
+                <img src="{{ asset('/assets/frontsite/images/hero-image.png') }}" class="bg-cover bg-center object-cover object-center max-h-[580px]" alt="Hero Image"/>
+                <div class="text-center absolute bottom-0 -left-20 -translate-y-14 bg-white px-7 py-5 rounded-xl shadow-2xl">
+                    <h5 class="font-medium text-[#1E2B4F]">Dr. Kartika Me</h5>
+                    <p class="text-xs text-[#AFAEC3] mt-1">Nutrionist</h1>
+                    <span class="block text-xs text-[#1E2B4F] font-medium bg-[#F2F6FE] px-4 py-2 rounded-full text-center mt-7">Book Now</span>
+                </div>
+            </div>
+
+            <!-- Hero Description -->
+            <div class="relative mx-auto max-w-7xl px-4 lg:px-14 lg:py-16">
+                <div class="lg:grid lg:grid-cols-12 lg:gap-8">
+                    <div class="lg:col-span-6">
+
+                        <!-- Label New -->
+                        <h1>
+                            <div class="flex items-center">
+                                <span class="text-white text-xs sm:text-sm font-medium bg-[#2AB49B] rounded-full px-8 py-2">New</span>
+                                <span class="text-[#1E2B4F] text-[11px] sm:text-sm bg-[#F2F6FE] rounded-r-full px-8 py-2 relative -z-10 -ml-4">Emergency call feature updated</span>
+                            </div>
+
+                            <span class="mt-6 block text-4xl font-semibold sm:text-5xl">
+                                <span class="block text-[#1E2B4F] leading-normal">Meet Your Doctor. <br />Trusted & Professional.</span>
+                            </span>
+                        </h1>
+                        <!-- End Label New -->
+
+                        <!-- Text -->
+                        <div class="flex flex-wrap gap-16 mt-8">
+                            <div class="flex items-center gap-4">
+                                <div class="flex-shrink-0">
+                                    <img src="{{ asset('/assets/frontsite/images/service.svg') }}" alt="service icon"/>
+                                </div>
+                                <div>
+                                    <h5 class="text-[#1E2B4F] text-lg font-medium">Best Recipe</h5>
+                                    <p class="text-[#AFAEC3]">for your medicine</p>
+                                </div>
+                            </div>
+                            <div class="flex items-center gap-4">
+                                <div class="flex-shrink-0">
+                                    <img src="{{ asset('/assets/frontsite/images/service.svg') }}" alt="service icon"/>
+                                </div>
+                                <div>
+                                    <h5 class="text-[#1E2B4F] text-lg font-medium">Free Consultation</h5>
+                                    <p class="text-[#AFAEC3]">as we promised</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Text -->
+
+                        <!-- CTA Button -->
+                        <div class="grid lg:flex flex-wrap mt-20 gap-5">
+                            <a href="{{ route('register') }}" class="text-white text-lg font-medium text-center bg-[#0D63F3] rounded-full px-12 py-3">Sign Up</a>
+                            <a href="#" class="text-[#1E2B4F] text-lg font-medium text-center bg-[#F2F6FE] rounded-full px-16 py-3">Story</a>
+                        </div>
+                        <!-- CTA Button -->
+
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Hero -->
+
+    </main>
+    <!-- End Content -->
+
 @endsection
