5caff95abcd8435cfea49eb9df3c728f5281da16
Achmad Fauzi <achmadfauzi@live.com>
2022-05-01 14:06:03 +0700

    update

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -3,10 +3,15 @@
 namespace App\Actions\Fortify;
 
 use App\Models\User;
+use App\Models\ManagementAccess\DetailUser;
+
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 use Laravel\Jetstream\Jetstream;
+
+use Auth;
 
 class CreateNewUser implements CreatesNewUsers
 {
@@ -27,11 +32,26 @@ class CreateNewUser implements CreatesNewUsers
             // 'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
-        // update this section for after create user, system also create detail_user
-        return User::create([
-            'name' => $input['name'],
-            'email' => $input['email'],
-            'password' => Hash::make($input['password']),
-        ]);
+
+        return DB::transaction(function () use ($input) {
+            return tap(User::create([
+                'name' => $input['name'],
+                'email' => $input['email'],
+                'password' => Hash::make($input['password']),
+            ]), function (User $user) {
+
+                // add to detail users
+                $detail_user = new DetailUser;
+                $detail_user->user_id = $user->id;
+                $detail_user->type_user_id = 3;
+                $detail_user->contact = NULL;
+                $detail_user->address = NULL;
+                $detail_user->photo = NULL;
+                $detail_user->gender = NULL;
+                $detail_user->save();
+
+            });
+        });
+
     }
 }
